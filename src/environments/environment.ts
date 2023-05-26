1ad6b9bc0a4d0d5f667462b4cf246655d0deb48b
yuri512w <=yuri512w@gmail.com>
2023-05-26 10:46:30 -0300

    crud do cliente

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+<<<<<<< HEAD
+  apiClientes: 'http://localhost:8080/api/cliente'
+=======
+  apiClientes: 'http://localhost:8080/api/clientes'
+>>>>>>> 98c476e88ce393d70ab077f313219811875c9933
+};
